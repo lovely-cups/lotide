fa14fe77838a11787dac684b0f1ce243f0532639
‘Charles <charlesthompson.2693@gmail.com>
2020-09-16 23:58:55 +0000

    assessing key is matching value in object

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💥Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val){
+  return Object.keys(obj) .find(key => 
+    obj[key] === val);
+  
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "South Park"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
