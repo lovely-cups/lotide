8e5d61d44da685146fa2a90881adadf81ea97904
‘Charles <charlesthompson.2693@gmail.com>
2020-09-16 22:20:04 +0000

    account for number of letters in string

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💥Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const countLetters = function(str){
+const letterResult = {};
+for (const num of str){
+  if(letterResult[num]){
+    letterResult[num] += 1;
+  }else {
+    letterResult[num] = 1
+  }
+}
+return letterResult;
+}
+const resultOf = countLetters("sassafrass");
+console.log(resultOf);
