6e77cea7d604258d0bce1e6c264e2d905eea6dcb
‘Charles <charlesthompson.2693@gmail.com>
2020-09-17 23:16:37 +0000

    callback to map out first elements in array

---
+++ map.js
@@ -0,0 +1,30 @@
+const eqArrays = function(arr1, arr2){
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++){
+    if(arr2[i] !== arr1[i]) {
+      return false;
+    }
+    }
+  return true;
+}
+const assertArrayEquals = function(arr1, arr2){
+  console.log(arr1, arr2);
+  if (eqArrays(arr1, arr2)) {
+    console.log(`👍Assertion Passed `);
+  } else {
+    console.log(`💥Assertion Failed `);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback){
+
+  const results = [];
+  for (let item of array){
+    results.push(callback(item));
+  }
+return results;
+}
