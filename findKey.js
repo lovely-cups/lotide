fc655ff5883a8da399b7168b894c907d38531bc6
‘Charles <charlesthompson.2693@gmail.com>
2020-09-18 01:38:22 +0000

    find value in obj with callbacks

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`✋Assertion passed: ${actual} === ${expected}`);
+  }else {
+    console.log(`✊Assertion Failed: ${actual} !=== ${expected}`);
+  }
+ };
+
+ const findKey = function(obj, callback){
+   for(key in obj){
+     if (callback(obj[key])){
+      return key;
+    } 
+ }
+}
+
+
+ 
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+assertEqual(findKey(obj, key), true);
