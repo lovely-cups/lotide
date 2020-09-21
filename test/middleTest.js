37b1e5bec09d650a0d1cebc339305d0cda2a5405
‘Charles <charlesthompson.2693@gmail.com>
2020-09-21 23:29:05 +0000

    refactored M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,17 @@
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.notStrictEqual(middle([1, 2, 3]), 2);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.notStrictEqual(middle(['5']), '5'); 
+  });
+});
+/*const middle = require('../middle');
 const eqArrays = require('../eqArrays');
 const assertArrayEquals = require('../assertArrayEqual');
 
 assertArrayEquals(middle([1,2,3,4,5,6]), [3,4]);
+*/
