1f159ee9bbc6bbc23dde1f2ddcba44810e0c3893
‘Charles <charlesthompson.2693@gmail.com>
2020-09-21 23:28:41 +0000

    refactored for M & C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.notStrictEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.notStrictEqual(tail(['5']), '5'); 
+  });
+});
+
+/*const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const result = tail([1,2,3])
 assertEqual(result.length, 2);
 assertEqual(result[0], 2);
 assertEqual(result[1], 3);
+*/
