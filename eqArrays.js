00cdfbd2542b9f73e3790e8d111d8d5f2e5b98b1
‘Charles <charlesthompson.2693@gmail.com>
2020-09-21 22:11:56 +0000

    refactored for methods

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`💥Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
 
 const eqArrays = function(arr1, arr2){
   let result = false;
@@ -19,9 +12,5 @@ const eqArrays = function(arr1, arr2){
   }
   return result;
 }
-assertEqual(eqArrays([1, 2, "3", 4], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
