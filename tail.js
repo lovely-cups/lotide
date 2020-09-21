654010a4090c70b218e5091379a7ff38074b5f4d
‘Charles <charlesthompson.2693@gmail.com>
2020-09-21 22:06:18 +0000

    refactored for methods

--- tail.js
+++ tail.js
@@ -1,25 +1,8 @@
-const assertEqual = function(actual, expected) {
-  let num1 = actual;
-  let num2 = expected;
-  
-   if(actual === expected){
-    console.log(`✋Assertion passed: ${num1} === ${num2}`);
-  }else if(actual !== expected){
-    console.log(`✊Assertion Failed: ${num1} !=== ${num2}`);
-  }
+
+ const tail = function(words) {
+  let result = words.slice(1);
+  return result;
  };
-  
- const tail = (words) => {
-   let results = [];
-   for (let i =0; i < words.length; i++){
-     if (i > 0){
-       results.push(words[i])
-     }
-   }
-   return results;
- }
  //.slice()
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const arr = tail(words); 
-assertEqual(arr.length, 3);
+module.exports = tail;
